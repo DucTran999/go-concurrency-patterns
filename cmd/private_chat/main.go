dc62cbc5eb5fd8fa70e92cff988c75087c4c1980
Đức Trần <tranaduc9x@gmail.com>
2025-07-25 23:35:14 +0700

    refactor: use wg for wait conversaion done

--- cmd/private_chat/main.go
+++ cmd/private_chat/main.go
@@ -2,52 +2,68 @@ package main
 
 import (
 	"context"
+	"log"
 	"sync"
+	"time"
 
 	privatechat "github.com/DucTran999/go-concurrency-patterns/case_study/private_chat"
 )
 
-// This app used generator pattern for generating
-// the nonce a random number used for encrypt and decrypt message
-// in Bob and Alice conversation
 func main() {
+	// Communication channels
 	bobSentChan := make(chan string)
 	aliceSentChan := make(chan string)
 
-	bob := privatechat.NewPerson("Bob", []string{
-		"Hi, Alice",
-		"Would you like to join me for dinner at the restaurant tonight at 7:00 pm?",
-	}, bobSentChan, aliceSentChan, 0)
-	alice := privatechat.NewPerson("Alice", []string{
-		"Hi, Bob",
-		"Sounds great! See you at 7!",
-	}, aliceSentChan, bobSentChan, 1)
+	// Create Bob and Alice with their messages
+	bob := privatechat.NewPerson(
+		"Bob",
+		[]string{
+			"Hi, Alice",
+			"Would you like to join me for dinner at the restaurant tonight at 7:00 pm?",
+		},
+		bobSentChan, aliceSentChan, 0,
+	)
 
+	alice := privatechat.NewPerson(
+		"Alice",
+		[]string{
+			"Hi, Bob",
+			"Sounds great! See you at 7!",
+		},
+		aliceSentChan, bobSentChan, 1,
+	)
+
+	// Secret nonce generator
 	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
 
 	secretChan := privatechat.Generator(ctx)
+
+	// Fan-out the generated secrets to both Alice and Bob
 	go func() {
-		for s := range secretChan {
-			secret := s
-			bob.SecretChan <- secret
-			alice.SecretChan <- secret
+		for secret := range secretChan {
+			bob.ReceiveSecret(secret)
+			alice.ReceiveSecret(secret)
 		}
 	}()
 
+	// Start chatting
+	log.Println("[INFO] conversation start")
 	var wg sync.WaitGroup
+	wg.Add(2)
 
-	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		bob.Chat()
 	}()
 
-	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		alice.Chat()
 	}()
 
 	wg.Wait()
+
+	cancel()                           // signal to stop secret generator if it's still running
+	time.Sleep(100 * time.Millisecond) // Give time for graceful closing
+	log.Println("[INFO] conversation ended.")
 }
